cdcff35983ed44fb52175cc51cfd3dc4a4b0d437
John Marcus III <j3@jmarcus.com>
2018-07-25 18:15:30 -0400

    adding refresh component

--- rpi_dash/data_handler.py
+++ rpi_dash/data_handler.py
@@ -1,4 +1,6 @@
-import json, os
+import json
+import os
+import time, datetime
 
 import requests
 
@@ -14,18 +16,41 @@ class Data_Handler():
         self.DATA_PATHS['weather'] = 'rpi_dash/static/data/forecast.json'
         self.API_PATHS['weather'] = 'https://api.darksky.net/forecast/' + os.environ['WEATHER_API_KEY'] + '/42.37,-71.0828'
 
+    def _refresh_external_weather_data(self):
+        # print("INFO {}: Firing off weather data request".format(datetime.datetime.now()))
+
         r = requests.get(
             self.API_PATHS['weather']
         )
 
         # Set initial weather data
-        self.weather_data = r.json()
+        w_json = r.json()
+        w_json['last_refresh_time'] = int(time.time())
+
+        # write to hard drive to pick up later
+        with open(self.DATA_PATHS['weather'], 'w') as f:
+            json.dump(w_json, f)
+
+        self.weather_data = w_json
+        return w_json
 
     def get_data(self):
-        return self.weather_data
+        w_json = None
 
-    # def load_local_data(self, path):
-    #     payload = {}
-    #     with open(path) as f:
-    #         payload = json.load(f)
-    #     return payload
+        if os.path.isfile(self.DATA_PATHS['weather']):
+            with open(self.DATA_PATHS['weather']) as f:
+                w_json = json.load(f)
+
+        # no file
+        if w_json is None:
+            w_json = self._refresh_external_weather_data()
+
+        # no last_refresh_time set
+        if 'last_refresh_time' not in w_json.keys():
+            w_json = self._refresh_external_weather_data()
+
+        # old data - this can dial down to 6 seconds and not cost at this point
+        if (int(time.time()) - w_json['last_refresh_time']) > 60:
+            w_json = self._refresh_external_weather_data()
+
+        return w_json
---
+++ rpi_dash/static/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // Refresh the page every 5 minutes
+  setInterval(function () {
+    window.location.reload(true);
+  }, 300000);
+});
