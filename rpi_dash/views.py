a2ceba1e091e9ab66268de8e4ac79d620782a0bc
John Marcus III <j3@jmarcus.com>
2018-08-06 08:56:04 -0400

    checkpoint, route to add IP

--- rpi_dash/views.py
+++ rpi_dash/views.py
@@ -3,15 +3,24 @@
 # themselves and grab models as needed to pass information
 # through this business logic layer.
 
-from flask import render_template
-from rpi_dash import app
-# TODO: this feels dirty, not sure why
-from rpi_dash.data_handler import Data_Handler
+from flask import render_template, request
+from rpi_dash import app, data_handler, mongo
 
-handler = Data_Handler()
+handler = data_handler.Data_Handler()
 
 @app.route('/')
 def index():
-    # payload = handler.load_local_data('rpi_dash/static/data/forecast.json')
     payload = handler.get_data()
     return render_template('index.html', payload=payload)
+
+@app.route('/records/')
+def get_documents():
+    records = mongo.db.measurements.find({})
+    return render_template('records.html', records=records)
+
+@app.route('/records/add/', methods=['POST'])
+def insert_document():
+    new_doc = request.form['number']
+    print(new_doc)
+    return render_template('base.html')
+    # number = request.form['']
