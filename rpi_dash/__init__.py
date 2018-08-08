0048a5f916875cf52ca5822a2cd415609dc02dea
John <j3@jwmarcus.com>
2018-08-08 17:27:22 -0400

    add mongo, uwsgi support (#1)
    * checkpoint, route to add IP
    
    * adding mongo endpoint, uwsgi template

--- rpi_dash/__init__.py
+++ rpi_dash/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-import rpi_dash.views
+# MongoDB setup
+app.config["MONGO_URI"] = "mongodb://localhost:27017/iotdata"
+mongo = PyMongo(app)
 
 # Circular import, chill out, it's ok.
 # This is here to make sure everything in the views.py is here
 # if we need it. Here for more:
 # http://flask.pocoo.org/docs/1.0/patterns/packages/
+import rpi_dash.views
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
